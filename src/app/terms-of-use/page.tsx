ad473ac630f924dd52020719a41be227a54c796d
Ian Nerney <ian@iannerney.com>
2023-12-13 16:29:54 -0500

    Update Text to Paragraph

--- src/app/terms-of-use/page.tsx
+++ src/app/terms-of-use/page.tsx
@@ -2,7 +2,7 @@
 import { PageLayout } from "@/components/Layouts";
 import Link from "next/link";
 import { Typography } from "antd";
-const { Title, Text } = Typography;
+const { Title, Paragraph } = Typography;
 
 const TermsOfUsePage = () => {
     return (
@@ -12,59 +12,55 @@ const TermsOfUsePage = () => {
 
                 <Title level={2}>1. Terms</Title>
 
-                <Text>
+                <Paragraph>
                     By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of
                     Use, all applicable laws and regulations, and agree that you are responsible for compliance with any
                     applicable local laws. If you do not agree with any of these terms, you are prohibited from using or
                     accessing this site. The materials contained in this web site are protected by applicable copyright
                     and trade mark law.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>2. Use License</Title>
 
-                <Text>
+                <Paragraph>
                     The MIT License applies to the use and distribution of this software.
                     <blockquote>
-                        <Text>Copyright © 2017-{new Date().getFullYear()} IMN Services, LLC</Text>
-                        <Text>
+                        <Paragraph>Copyright © 2017-{new Date().getFullYear()} IMN Services, LLC</Paragraph>
+                        <Paragraph>
                             Permission is hereby granted, free of charge, to any person obtaining a copy of this
                             software and associated documentation files (the &quot;Software&quot;), to deal in the
                             Software without restriction, including without limitation the rights to use, copy, modify,
                             merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
                             persons to whom the Software is furnished to do so, subject to the following conditions:
-                        </Text>
-                        <Text>
+                        </Paragraph>
+                        <Paragraph>
                             The above copyright notice and this permission notice shall be included in all copies or
                             substantial portions of the Software.
-                        </Text>
-                        <Text>
+                        </Paragraph>
+                        <Paragraph>
                             THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
                             IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
                             PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
                             BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
                             OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
                             DEALINGS IN THE SOFTWARE.
-                        </Text>
+                        </Paragraph>
                     </blockquote>
-                </Text>
+                </Paragraph>
                 <Title level={2}>3. Disclaimer</Title>
-
-                <ol type="a">
-                    <li>
-                        The materials on this web site are provided &quot;as is&quot;. IMN Services, LLC and related
-                        parties make no warranties, expressed or implied, and hereby disclaims and negates all other
-                        warranties, including without limitation, implied warranties or conditions of merchantability,
-                        fitness for a particular purpose, or non-infringement of intellectual property or other
-                        violation of rights. Further, IMN Services, LLC and related parties do not warrant or make any
-                        representations concerning the accuracy, likely results, or reliability of the use of the
-                        materials on this Internet web site or otherwise relating to such materials or on any sites
-                        linked to this site.
-                    </li>
-                </ol>
+                <Paragraph>
+                    The materials on this web site are provided &quot;as is&quot;. IMN Services, LLC and related parties
+                    make no warranties, expressed or implied, and hereby disclaims and negates all other warranties,
+                    including without limitation, implied warranties or conditions of merchantability, fitness for a
+                    particular purpose, or non-infringement of intellectual property or other violation of rights.
+                    Further, IMN Services, LLC and related parties do not warrant or make any representations concerning
+                    the accuracy, likely results, or reliability of the use of the materials on this Internet web site
+                    or otherwise relating to such materials or on any sites linked to this site.
+                </Paragraph>
 
                 <Title level={2}>4. Limitations</Title>
 
-                <Text>
+                <Paragraph>
                     In no event shall IMN Services, LLC or related parties be liable for any damages (including, without
                     limitation, damages for loss of data or profit, or due to business interruption,) arising out of the
                     use or inability to use the materials on IMN Services, LLC&apos;s Internet site, even if IMN
@@ -72,63 +68,63 @@ const TermsOfUsePage = () => {
                     possibility of such damage. Because some jurisdictions do not allow limitations on implied
                     warranties, or limitations of liability for consequential or incidental damages, these limitations
                     may not apply to you.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>5. Revisions and Errata</Title>
 
-                <Text>
+                <Paragraph>
                     The materials appearing on IMN Services, LLC&apos;s web site could include technical, typographical,
                     or photographic errors. IMN Services, LLC does not warrant that any of the materials on its web site
                     are accurate, complete, or current. IMN Services, LLC may make changes to the materials contained on
                     its web site at any time without notice. IMN Services, LLC does not, however, make any commitment to
                     update the materials.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>6. Links</Title>
 
-                <Text>
+                <Paragraph>
                     IMN Services, LLC has not reviewed all of the sites linked to its Internet web site and is not
                     responsible for the contents of any such linked site. The inclusion of any link does not imply
                     endorsement by IMN Services, LLC of the site. Use of any such linked web site is at the user&apos;s
                     own risk.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>7. Site Terms of Use Modifications</Title>
 
-                <Text>
+                <Paragraph>
                     IMN Services, LLC may revise these terms of use for its web site at any time without notice. By
                     using this web site you are agreeing to be bound by the then current version of these Terms and
                     Conditions of Use.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>8. Governing Law</Title>
 
-                <Text>
+                <Paragraph>
                     Any claim relating to IMN Services, LLC&apos;s web site shall be governed by the laws of the State
                     of Florida without regard to its conflict of law provisions.
-                </Text>
+                </Paragraph>
             </section>
 
             <section id="privacy-policy">
                 <Title level={1}>Privacy Policy</Title>
 
-                <Text>
+                <Paragraph>
                     Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to
                     understand how we collect, use, communicate and disclose and make use of personal information. The
                     following outlines our privacy policy.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>Cookies</Title>
 
-                <Text>
+                <Paragraph>
                     We may use cookies in order to customize this site for return visitors. These cookies are not
                     required for site functionality. Additionally, third-party widgets may install cookies depending on
                     their configuration. You are not required to accept any cookies to use this site.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>Google Analytics</Title>
 
-                <Text>
+                <Paragraph>
                     We use a tool called “Google Analytics” to collect information about use of this site. Google
                     Analytics collects information such as how often users visit this site, what pages they visit when
                     they do so, and what other sites they used prior to coming to this site. We use the information we
@@ -148,11 +144,11 @@ const TermsOfUsePage = () => {
                     </Link>
                     . You can prevent Google Analytics from recognizing you on return visits to this site by disabling
                     cookies on your browser.
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>Hotjar Tracking</Title>
 
-                <Text>
+                <Paragraph>
                     We use{" "}
                     <Link href="http://www.hotjar.com" rel="nofollow">
                         Hotjar
@@ -172,27 +168,27 @@ const TermsOfUsePage = () => {
                         this link
                     </Link>
                     .
-                </Text>
+                </Paragraph>
 
-                <Text>
+                <Paragraph>
                     You can opt-out to the creation of a user profile, Hotjar&apos;s storing of data about your usage of
                     our site and Hotjar&apos;s use of tracking cookies on other websites by following this{" "}
                     <Link href="https://www.hotjar.com/legal/compliance/opt-out" rel="nofollow">
                         opt-out link
                     </Link>
                     .
-                </Text>
+                </Paragraph>
 
                 <Title level={2}>Questions?</Title>
 
-                <Text>
+                <Paragraph>
                     If you have any questions about use of your data, or if you have any privacy concerns relating to
                     your use of this website, please contact us at{" "}
                     <Link href="mailto:contact@skywaybridgestatus.com" rel="nofollow">
                         contact@skywaybridgestatus.com
                     </Link>
                     .
-                </Text>
+                </Paragraph>
             </section>
         </PageLayout>
     );
--- src/components/Blocks/ProjectInformation/ProjectInformation.tsx
+++ src/components/Blocks/ProjectInformation/ProjectInformation.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Row, Col, Typography } from "antd";
-const { Title, Text, Link } = Typography;
+const { Title, Paragraph } = Typography;
 
 const ProjectInformation = () => {
     return (
@@ -8,22 +8,22 @@ const ProjectInformation = () => {
             <Row gutter={[48, 48]}>
                 <Col xs={24} md={16}>
                     <Title level={2}>About</Title>
-                    <Text>
+                    <Paragraph>
                         SkywayBridgeStatus.com is a project aimed at helping Tampa Bay commuters plan for unexpected
                         closures of the Sunshine Skyway Bridge. This web application actively collects real-time weather
                         and traffic data from reputable sources. Our goal is to use this historical data to create a
                         closure prediction model available to the public.
-                    </Text>
+                    </Paragraph>
                 </Col>
                 <Col xs={24} md={8}>
                     <Title level={2}>Sponsor This Project</Title>
-                    <Text>
+                    <Paragraph>
                         If you’re interested in contributing to, or sponsoring this project, please visit our{" "}
                         <a href="https://github.com/iannerney/skywaybridgestatus">GitHub page</a>,{" "}
                         <a href="mailto:contribute@skywaybridgestatus.com">email us</a>, or{" "}
                         <a href="https://www.buymeacoffee.com/iannerney">make a donation</a> to help keep our web
                         servers running.
-                    </Text>
+                    </Paragraph>
                 </Col>
             </Row>
         </section>
