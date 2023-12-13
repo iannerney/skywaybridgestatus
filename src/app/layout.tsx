4608752fd191a190f43b3a0b38cd7b4b30ed3f7e
Ian Nerney <ian@iannerney.com>
2023-12-13 06:51:56 -0500

    Resolve build error with Firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,27 +4,28 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "../../public/antd.min.css";
 
-// // Initialize Firebase
-// import { initializeApp } from "firebase/app";
-// import { firebaseConfig } from "../../site.config";
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../../site.config";
+const app = initializeApp(firebaseConfig);
 
-// // Monitor auth state
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// import { PageLayout } from "@/components/Layouts";
+// Monitor auth state
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { PageLayout } from "@/components/Layouts";
 
-// const auth = getAuth();
-// onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//         // User is signed in, see docs for a list of available properties
-//         // https://firebase.google.com/docs/reference/js/auth.user
-//         const uid = user.uid;
-//         // ...
-//     } else {
-//         // User is signed out
-//         // ...
-//     }
-// });
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
+
 export const metadata: Metadata = {
     title: `${branding.siteName} - ${branding.siteTagline}`,
     description: branding.siteDescription,
