bda5bcd56c7453587aebee0a5af301da69843c01
Ian Nerney <ian@iannerney.com>
2023-12-20 11:12:53 -0500

    Removing Firebase from app in prep for native

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,27 +13,6 @@ export const viewport: Viewport = {
     themeColor: "#ffffff",
 };
 
-// Initialize Firebase
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "../../site.config";
-const app = initializeApp(firebaseConfig);
-
-// Monitor auth state
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid;
-        // ...
-    } else {
-        // User is signed out
-        // ...
-    }
-});
-
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="en">
