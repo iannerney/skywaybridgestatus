4d0cbcd2a4c03ff1a7dc4e9d1ce70c8bbb1c61f5
Ian Nerney <ian@iannerney.com>
2023-12-21 09:47:11 -0500

    Add static export to build step

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        unoptimized: true,
-    },
     output: "export",
-    // trailingSlash: true,
-    // skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
