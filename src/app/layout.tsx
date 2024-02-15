9fd547ec1f29202c213359ae7682ee14a65cb933
Ian Nerney <ian@iannerney.com>
2024-02-15 11:50:52 -0500

    Add Google AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { branding, settings } from "site.config";
 import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Viewport } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     metadataBase: new URL(branding.canonicalUrlBase),
@@ -37,6 +38,11 @@ onAuthStateChanged(auth, (user) => {
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="en">
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8789676310669251"
+                crossOrigin="anonymous"
+            />
             <body>{children}</body>
             <GoogleTagManager gtmId={settings.googleTagManagerId} />
         </html>
