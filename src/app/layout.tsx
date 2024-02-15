1a0f31c634b22a1cfd967402fe123395d8eceabc
Ian Nerney <ian@iannerney.com>
2024-02-15 07:24:17 -0500

    Stashing WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import "./globals.css";
 import "../../public/antd.min.css";
 import { branding, settings } from "site.config";
 import { GoogleTagManager } from "@next/third-parties/google";
-import type { Metadata } from "next";
-import { Viewport } from "next";
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
     metadataBase: new URL(branding.canonicalUrlBase),
